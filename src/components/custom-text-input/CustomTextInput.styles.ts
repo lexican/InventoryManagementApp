aee4c9d9516c07ba38d1af19c895fe2fdb1bc846
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-03-12 13:49:06 +0100

    feat : CustomTextInput

---
+++ src/components/custom-text-input/CustomTextInput.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const TextInputField = styled.TextInput`
+  width: 100%;
+  background: #ffffff;
+  height: 44px;
+  padding: 10px;
+  border-width: 1px;
+  border-color: black;
+  margin-bottom: 12px;
+  border-radius: 12px;
+`;
---
+++ src/components/custom-text-input/CustomTextInput.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {KeyboardTypeOptions} from 'react-native/types';
+import {TextInputField} from './CustomTextInput.styles';
+
+type IProps = {
+  secureTextEntry?: boolean;
+  placeholder?: string;
+  value?: string;
+  keyboard: KeyboardTypeOptions | undefined;
+  onChangeText?: (text: string) => void;
+};
+
+const CustomTextInput = ({
+  secureTextEntry = false,
+  placeholder,
+  value,
+  onChangeText,
+  keyboard = 'default',
+}: IProps) => {
+  return (
+    <TextInputField
+      secureTextEntry={secureTextEntry}
+      placeholder={placeholder}
+      value={value}
+      onChangeText={onChangeText}
+      keyboardType={keyboard}
+    />
+  );
+};
+
+export default CustomTextInput;
