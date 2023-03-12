5907903aaaa3edda4f5d541053efbdb4939ff04e
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-03-12 17:19:45 +0100

    feat : Inventory EmptyState

--- src/components/inventory-list/Inventory.styles.ts
+++ src/components/inventory-list/Inventory.styles.ts
@@ -4,3 +4,23 @@ export const InventoryList = styled.View`
   flex: 1;
   height: 100%;
 `;
+
+export const EmptyStateContainer = styled.View`
+  height: 60%;
+  justify-content: center;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const EmptyStateText = styled.Text`
+  color: #000000;
+  font-size: 24px;
+  text-align: center;
+`;
+
+export const EmptyStateSubText = styled.Text`
+  color: #212121;
+  font-size: 16px;
+  text-align: center;
+  margin-top: 20px;
+`;
--- src/components/inventory-list/InventoryList.tsx
+++ src/components/inventory-list/InventoryList.tsx
@@ -3,9 +3,24 @@ import React from 'react';
 import {IInventory} from '../../types/types';
 import InventoryItem from '../inventory-item/InventoryItem';
 import {useInventoryStateContext} from '../../contexts/inventory-context/InventoryContext';
+import {
+  EmptyStateContainer,
+  EmptyStateSubText,
+  EmptyStateText,
+} from './Inventory.styles';
 
 const InventoryList = () => {
   const {inventories} = useInventoryStateContext();
+  if (inventories.length == 0) {
+    return (
+      <EmptyStateContainer>
+        <EmptyStateText>Inventory is Empty</EmptyStateText>
+        <EmptyStateSubText>
+          Add an Inventory by clicking on the create button above
+        </EmptyStateSubText>
+      </EmptyStateContainer>
+    );
+  }
   return (
     <FlatList<IInventory>
       numColumns={1}
